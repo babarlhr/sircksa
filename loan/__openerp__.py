81f287e5a5695596f8809109c2385d3616768f74
Ecube <danish_ali89@hotmail.com>
2020-01-21 18:33:50 +0500

    Saudi Fast

---
+++ loan/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "loan",
+
+    'summary': """
+        Long description of module's purpose""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Wahab Ali Malik",
+    'website': "http://bcube.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base','hr'],
+    'data': [
+        'views/views.xml',
+    ],
+} 
